17c6d05e7c8b4b06b9f1d46f86f3b8c47376ac8c
Kai Waelti <0x6b57@pm.me>
2025-03-25 23:12:05 +0100

    Disable copilot

--- lua/kw/plugins/lsp/none-ls.lua
+++ lua/kw/plugins/lsp/none-ls.lua
@@ -1,14 +1,12 @@
 return {
-    "nvimtools/none-ls.nvim",
-    config = function()
-        local null_ls = require("null-ls")
-
-        null_ls.setup({
-            sources = {
-                null_ls.builtins.formatting.stylua,
-            },
-        })
-
-        vim.keymap.set("n", "<leader>vff", vim.lsp.buf.format, { desc = "Format current file" })
-    end,
+    -- "nvimtools/none-ls.nvim",
+    -- config = function()
+    --     local null_ls = require("null-ls")
+    --
+    --     null_ls.setup({
+    --         sources = {
+    --             null_ls.builtins.diagnostics.ruff
+    --         },
+    --     })
+    -- end,
 }
