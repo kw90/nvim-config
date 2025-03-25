838bf4fd047a15cea1c0f67fa4bac99f5b9ad8d8
Kai Waelti <0x6b57@pm.me>
2025-03-25 23:13:51 +0100

    Configure devcontainers

--- lua/kw/plugins/nvim-devcontainer-cli.lua
+++ lua/kw/plugins/nvim-devcontainer-cli.lua
@@ -4,11 +4,12 @@ return {
 		-- By default, if no extra config is added, following nvim_dotfiles are
 		-- installed: "https://github.com/LazyVim/starter"
 		-- This is an example for configuring other nvim_dotfiles inside the docker container
-		nvim_dotfiles_repo = "https://github.com/arnaupv/dotfiles.git",
-		nvim_dotfiles_install_command = "cd ~/nvim_dotfiles/ && ./install.sh",
+		nvim_dotfiles_repo = "https://github.com/kw90/nvim-config.git",
+		-- nvim_dotfiles_install_command = "cd ~/nvim_dotfiles/ && ./install.sh",
 		-- In case you want to change the way the devenvironment is setup, you can also provide your own setup
-		setup_environment_repo = "https://github.com/arnaupv/setup-environment",
-		setup_environment_install_command = "./install.sh -p 'nvim stow zsh'",
+		-- setup_environment_repo = "https://github.com/arnaupv/setup-environment",
+		-- setup_environment_install_command = "./install.sh -p 'nvim stow zsh'",
+        devcontainer_folder = "."
 	},
 	keys = {
         -- stylua: ignore
