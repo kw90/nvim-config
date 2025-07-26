81ef0b902b4862181dedc32adf38a50410037885
Kai Waelti <hi@kaiwaelti.me>
2025-07-26 17:16:00 +0200

    Set up python refactoring using ropevim

--- lua/kw/plugins/ropevim.lua
+++ lua/kw/plugins/ropevim.lua
@@ -1,7 +1,19 @@
-
 return {
     "python-rope/ropevim",
-    opts = {
-        -- options
-    },
+    ft = "python",
+    build = "pip install rope ropevim pynvim",
+    config = function()
+        vim.g.ropevim_enable_shortcuts = 0
+        -- Rope refactoring keymaps
+        vim.keymap.set('n', '<leader>rm', ':RopeExtractMethod<CR>', { desc = "Rope: Extract method" })
+        vim.keymap.set('n', '<leader>rv', ':RopeExtractVariable<CR>', { desc = "Rope: Extract variable" })
+        vim.keymap.set('n', '<leader>ri', ':RopeInline<CR>', { desc = "Rope: Inline" })
+        vim.keymap.set('n', '<leader>rM', ':RopeMove<CR>', { desc = "Rope: Move" })
+        vim.keymap.set('n', '<leader>rs', ':RopeRestructure<CR>', { desc = "Rope: Restructure" })
+        vim.keymap.set('n', '<leader>ro', ':RopeOrganizeImports<CR>', { desc = "Rope: Organize imports" })
+        vim.keymap.set('n', '<leader>rg', ':RopeGenerateFunction<CR>', { desc = "Rope: Generate function" })
+        vim.keymap.set('n', '<leader>rc', ':RopeChangeSignature<CR>', { desc = "Rope: Change signature" })
+        vim.keymap.set('n', '<leader>ru', ':RopeUndo<CR>', { desc = "Rope: Undo refactoring" })
+        vim.keymap.set('n', '<leader>rr', ':RopeRedo<CR>', { desc = "Rope: Redo refactoring" })
+    end,
 }
